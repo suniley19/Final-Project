707654353599f866d44b09c5e3923b97aa84c435
suniley19 <suniley19@gmail.com>
2025-12-07 23:30:01 -0500

    connected mongodb  database to the server

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports = async function () {
 try {
-await mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-});
+await mongoose.connect(process.env.MONGO_URI);
 console.log('MongoDB connected');
 } catch (err) {
 console.error(err.message);
