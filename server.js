5abe6e3f9d0bab91b503454aa091377852bfd39d
suniley19 <suniley19@gmail.com>
2025-12-07 22:44:03 -0500

    Started with  basic foundations for the project

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./config/db');
+
+
+const app = express();
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+
+// serve uploads statically
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+// routes
+app.use('/api', require('./routes/auth'));
+app.use('/api', require('./routes/files'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
